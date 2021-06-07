836784ac076f29a4a19849316737ba36c2526e02
evgenia-pospelova <72083483+evgenia-pospelova@users.noreply.github.com>
2021-06-07 20:31:37 +0200

    Update OS_LAB5_2.cpp

--- OS_LAB5_2.cpp
+++ OS_LAB5_2.cpp
@@ -1,13 +1,12 @@
 // OS_LAB5.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-#include <fstream>
 #include <pthread.h>
 #include <semaphore.h>
-#include <string.h>
 #include <fcntl.h>
-#include <windows.h>
+#include <unistd.h>
+
+
 
 
 
@@ -15,41 +14,36 @@
 using namespace std;
 
 bool flag = true;
-sem_t sem;
+sem_t* sem;
+FILE* fileName;
 
-void* func1(void* arg) {
-	int num = 2;
-	ofstream fout("newFile.txt", ios_base::app);
+void* func1(void* ) {
+	const char num = '2';
 	while (flag) {
-		sem_wait(&sem);
-		if (fout.is_open()) {
+		sem_wait(sem);
 			for (int i = 0; i < 5; i++) {
 				if (!flag) {
 					break;
 				}
-				fout << num;
-				cout << num << " ";
-				cout.flush();
-				Sleep(1);
-			}
-		}
-		else {
-			cout << "File does not exist!";
-		}
-		sem_post(&sem);
-		Sleep(1);
-		}
-		fout.close();
-		return NULL;
+				fputs(&num,fileName);
+                cout << num << endl;
+                sleep(1);
+            }
+		
+		sem_post(sem);
+		sleep(1);		
 	}
+    return NULL;
+}
 int main() {
 	sem_open("/sem", O_CREAT, 0644, 1);
+    fileName = fopen("test.txt","a"); //"a" - дописывает инф. к конфу файла
 	pthread_t thread1;
 	pthread_create(&thread1, NULL, func1, NULL);
 	flag = false;
 	pthread_join(thread1, NULL);
-	sem_close(&sem);
+	sem_close(sem);
 	sem_unlink("/sem");
+    fclose(fileName);
 	return 0;
 }
-
