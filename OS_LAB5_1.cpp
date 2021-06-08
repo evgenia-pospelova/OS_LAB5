25010f7dcd0b9a69c40890af9691817b51de6283
evgenia-pospelova <72083483+evgenia-pospelova@users.noreply.github.com>
2021-06-08 12:32:44 +0200

    Update OS_LAB5_1.cpp

--- OS_LAB5_1.cpp
+++ OS_LAB5_1.cpp
@@ -37,7 +37,7 @@ void* func1(void* ) {
     return NULL;
 }
 int main() {
-	sem_open("/sem", O_CREAT, 0644, 1);
+	sem_open("/sem", O_CREAT, 0644, 10);
     fileName = fopen("test.txt","a"); //"a" - дописывает инф. к конфу файла
 	pthread_t thread1;
 	pthread_create(&thread1, NULL, func1, NULL);
